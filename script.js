ec42215d62f20118f49db71bc8528e657845584d
Portfolio <portfolio@local>
2026-09-01 18:19:13 +0800

    Show all virtual IP designs in lively grid

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const revealObserver = new IntersectionObserver((entries) => {
 
 revealEls.forEach((el) => revealObserver.observe(el));
 
-const imageGalleries = document.querySelectorAll('.gallery-grid, .gallery-track');
+const imageGalleries = document.querySelectorAll('.gallery-grid:not(.ip-grid), .gallery-track');
 
 function initCarousel(carousel) {
   const viewport = carousel.querySelector('.carousel-viewport');
